48691293f877aa807159105cf2e443af8bc0b54f
Bastiaan Quast <bquast@gmail.com>
2022-12-13 03:40:24 -0600

    use simpler example for GenPolyMod

--- R/GenPolyMod.R
+++ R/GenPolyMod.R
@@ -3,5 +3,8 @@
 #' @param n the order
 #' @return polynomial of the form x^^n + 1
 #' @export
+#' @examples
+#' n = 5
+#' GenPolyMod(5)
 GenPolyMod <- function(n)
   polynomial( coef=c(1, rep(0, n-1), 1 ) )
