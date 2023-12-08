305937a653469eb0d4715b4a739f4345ce692240
Bastiaan Quast <bquast@gmail.com>
2023-12-08 06:14:31 -0600

    add GenPolyMod test

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,17 +1,20 @@
 library(testthat)
 
+# Ternary
 set.seed(123)
-
 ternary <- GenTernary(5)
 expect_equal(as.vector(ternary)[5],1)
 
-
+# Gaussian
 set.seed(123)
-
 gauss <- GenDiscrGauss(5)
 expect_equal(as.vector(gauss)[1],-1)
 
-
-set.seed(123)
+# CoefMod - Coefficient Modulo
 p <- polynomial(c(5, 3, 6))
 expect_equal(CoefMod(p,5)[2],3)
+
+# PolyMod
+set.seed(123)
+p <- GenPolyMod(5)
+expect_equal(sum(p[]),2)
